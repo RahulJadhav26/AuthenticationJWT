112ea2774f6708a99759ffa3903b4543409f94cc
Rahul Jadhav <rrahuljadhav.rj@gmail.com>
2020-10-13 16:25:03 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://Rahul:Raul1234@authentication.enbid.mongodb.net/users?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://Rahul:<password>@authentication.enbid.mongodb.net/<dbname>?retryWrites=true&w=majority',
   secret: 'yoursecretkey',
 };
