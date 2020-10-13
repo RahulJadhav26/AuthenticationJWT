9844df76a7627c4dd39cc9ee9d3839959e8debdb
RahulJadhav2611 <rrahuljadhav.rj@gmail.com>
2020-10-13 15:36:45 +0530

    mongodb

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://localhost:27017/auth',
+  mongoURI: 'mongodb+srv://Rahul:Raul1234@authentication.enbid.mongodb.net/users?retryWrites=true&w=majority',
   secret: 'yoursecretkey',
 };
